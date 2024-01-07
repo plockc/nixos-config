6f4a8d4f7f9db85c340a303317490c76a6e20688
Chris Plock <plockc@gmail.com>
2024-01-06 16:43:39 -0800

    added dropbox

--- linux-main.nix
+++ linux-main.nix
@@ -187,6 +187,7 @@ in {
     zig # systems programming language
     # see overlay above for electron
     unstable.obsidian # note taking
+    dropbox-cli
   ];
 
   services.kubernetes.roles = [ "master" "node" ];
@@ -227,4 +228,21 @@ in {
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.05"; # Did you read the comment?
 
+  systemd.user.services.dropbox = {
+    description = "Dropbox";
+    wantedBy = [ "graphical-session.target" ];
+    environment = {
+      QT_PLUGIN_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtPluginPrefix;
+      QML2_IMPORT_PATH = "/run/current-system/sw/" + pkgs.qt5.qtbase.qtQmlPrefix;
+    };
+    serviceConfig = {
+      ExecStart = "${pkgs.dropbox.out}/bin/dropbox";
+      ExecReload = "${pkgs.coreutils.out}/bin/kill -HUP $MAINPID";
+      KillMode = "control-group"; # upstream recommends process
+      Restart = "on-failure";
+      PrivateTmp = true;
+      ProtectSystem = "full";
+      Nice = 10;
+    };
+  };
 }
